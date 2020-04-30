54df4e327666ba71f668dbe00a78fc1ad01db198
Shen Qu <qushen024@hotmail.com>
2020-04-30 18:43:57 -0400

    Show Page

--- app.js
+++ app.js
@@ -18,14 +18,16 @@ app.set("view engine", "ejs");
 
 const campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 const Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
-//     name: "Mountain Goat's Rest",
-//     image: "https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e5074417d2d7fdd914cc2_340.jpg"
+//     name: "Granite Hill",
+//     image: "https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e5074417d2d7fdd914cc2_340.jpg",
+//     description: "This is a huge granite hill, no bacthrooms. No water. Beautiful granitel!"
 //     })
 //     .then((campground)=>{
 //         console.log("Newly created campground: ");
@@ -48,7 +50,7 @@ app.get("/campgrounds", (req,res)=>{
                 image: campground.image
             }
         });
-        res.render("campgrounds", {
+        res.render("index", {
             campgrounds: filteredCampgrounds
         });
     })
@@ -58,7 +60,8 @@ app.get("/campgrounds", (req,res)=>{
 app.post("/campgrounds", (req,res)=>{
     const newCampground = {
         name: req.body.name, 
-        image: req.body.image
+        image: req.body.image,
+        description: req.body.description
     };
     const campground = new Campground(newCampground);
     campground.save()
@@ -70,6 +73,17 @@ app.post("/campgrounds", (req,res)=>{
 
 app.get("/campgrounds/new", (req,res)=>{
     res.render("new");
+});
+
+app.get("/campgrounds/:id", (req,res)=>{
+
+    Campground.findById(req.params.id)
+    .then((campground)=>{
+        res.render("show", {
+            campground
+        });
+    })
+    .catch(err=>console.log(err));
 });
 
 
