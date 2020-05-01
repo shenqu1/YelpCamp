7bbeb393223685eb6528621c0ccdaa6c6d1774af
Shen Qu <qushen024@hotmail.com>
2020-05-01 18:39:07 -0400

    User model and authentication

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+
 Campground = require("./models/campground");
 Comment = require("./models/comment");
+User = require("./models/User");
 
 seedDB = require("./seeds");
 
@@ -16,6 +21,21 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(session({
+    secret: `${process.env.SECRET}`,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next)=>{
+    res.locals.currentUser = req.user;
+    next();
+});
 
 mongoose.connect(process.env.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
@@ -78,7 +98,7 @@ app.get("/campgrounds/:id", (req,res)=>{
     .catch(err=>console.log(err));
 });
 
-app.get("/campgrounds/:id/comments/new", (req,res)=>{
+app.get("/campgrounds/:id/comments/new", isLoggedIn, (req,res)=>{
     Campground.findById(req.params.id)
     .then((campground)=>{
         res.render("comments/new", {campground});
@@ -87,7 +107,7 @@ app.get("/campgrounds/:id/comments/new", (req,res)=>{
 
 });
 
-app.post("/campgrounds/:id/comments", (req,res)=>{
+app.post("/campgrounds/:id/comments",isLoggedIn, (req,res)=>{
     Campground.findById(req.params.id)
     .then((campground)=>{
         Comment.create(req.body.comment)
@@ -107,6 +127,46 @@ app.post("/campgrounds/:id/comments", (req,res)=>{
     });
 });
 
+app.get("/register", (req,res)=>{
+    res.render("register");
+});
+
+app.post("/register", (req,res)=>{
+    const newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password)
+    .then((user)=>{
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.render("register");
+    });
+});
+
+app.get("/login", (req,res)=>{
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), (req,res)=>{
+});
+
+app.get("/logout", (req,res)=>{
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
