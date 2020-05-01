ba2d08e622f934fd3138fb32b805cd099a5dcf95
Shen Qu <qushen024@hotmail.com>
2020-05-01 19:43:06 -0400

    User associations: Comment

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,39 +1,49 @@
-const express= require("express");
-const router = express.Router({mergeParams: true});
+const express = require("express");
+const router = express.Router({
+    mergeParams: true
+});
 const Campground = require("../models/campground");
 const Comment = require("../models/comment");
 
-router.get("/new", isLoggedIn, (req,res)=>{
+router.get("/new", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id)
-    .then((campground)=>{
-        res.render("comments/new", {campground});
-    })
-    .catch(err=>console.log(err));
+        .then((campground) => {
+            res.render("comments/new", {
+                campground
+            });
+        })
+        .catch(err => console.log(err));
 
 });
 
-router.post("/",isLoggedIn, (req,res)=>{
+router.post("/", isLoggedIn, (req, res) => {
     Campground.findById(req.params.id)
-    .then((campground)=>{
-        Comment.create(req.body.comment)
-        .then((comment)=>{
-            campground.comments.push(comment);
-            campground.save()
-            .then(()=>{
-                res.redirect(`/campgrounds/${campground._id}`);
-            })
-            .catch(err=>console.log(err));
+        .then((campground) => {
+            Comment.create(req.body.comment)
+                .then((comment) => {
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save()
+                        .then(() => {
+                            campground.comments.push(comment);
+                            campground.save()
+                                .then(() => {
+                                    res.redirect(`/campgrounds/${campground._id}`);
+                                })
+                                .catch(err => console.log(err));
+                        })
+                        .catch(err => console.log(err));
+                })
+                .catch(err => console.log(err));
         })
-        .catch(err=>console.log(err));
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.redirect("/campgrounds");
-    });
+        .catch((err) => {
+            console.log(err);
+            res.redirect("/campgrounds");
+        });
 });
 
 function isLoggedIn(req, res, next) {
-    if(req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
